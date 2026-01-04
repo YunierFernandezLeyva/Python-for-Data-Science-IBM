45ff78efff5cd8de42287e54ceabea7ec53505d3
Yunier Fernández Leyva <yunierfdezl@gmail.com>
2026-01-04 14:09:26 +0100

    Update ejercicios.py

--- ejercicios.py
+++ ejercicios.py
@@ -1,4 +1,4 @@
-temp= [6,9,3,3,3,5,7,7]
+'''temp= [6,9,3,3,3,5,7,7]
 def promedio(lista):
     return sum(lista)/len(lista)
 
@@ -7,3 +7,12 @@ print(promedio(temp))
 for i in range(len(temp)):
     if temp[i] > promedio(temp):
         print(f"El valor {temp[i]} en la posición {i} es mayor al promedio")
+'''
+
+temp = [6, 9, 3, 3, 3, 5, 7, 7]
+
+A = temp[0]
+B = temp[-1]
+print(A, B)
+suma = sum(temp)
+print(suma)
