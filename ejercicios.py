1bb620154a02c94f358c404f9091eede7df21742
Yunier Fernández Leyva <yunierfdezl@gmail.com>
2026-01-04 18:09:21 +0100

    Update ejercicios.py

--- ejercicios.py
+++ ejercicios.py
@@ -20,15 +20,21 @@ print(suma)'''
 # Ejercicio 1: Cast the following list to a set:
 ['A','B','C','A','B','C']
 set(['A','B','C','A','B','C'])
+
 # Ejercicio 2: Add the string 'D' to the set S.
 S = set(['A','B','C'])
 S={'A','B','C'}
 S.add('D')
 print(S)
+
 # Ejercicio 3: Find the intersection of set A and B.
-A={1,2,3,4,5}
-B={1,3,9, 12}
+
 A={1,2,3,4,5}
 B={1,3,9, 12}
 C = A & B
 print(C)
+
+# Ejercicio 4:  Create a set
+set1 = {"pop", "rock", "soul", "hard rock", "rock", "R&B", "rock", "disco"}
+set1
+print(set1)
