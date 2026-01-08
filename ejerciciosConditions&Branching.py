a50b36323afecb84a0bc1b4c9aaa384119ead95b
Yunier Fernández Leyva <yunierfdezl@gmail.com>
2026-01-08 20:47:20 +0100

    Update ejerciciosConditions&Branching.py

---
+++ ejerciciosConditions&Branching.py
@@ -0,0 +1,24 @@
+#---------------------------------Comparison Operations--------------------------------#
+
+#Find the value of i that produces a True:
+i=1
+i!=0  # True
+i==0  # False
+
+#---------------------------------Branching--------------------------------#
+
+#Find the value of x that prints the statement "this is a":
+
+x="a"
+if(x=='a'):
+    print("this is a")
+else:
+    print("this is  not a")
+
+#-------------------------------Logic Operators-------------------------------#
+
+#Find the value of y that produces a True statement:
+
+y=1
+x=1
+x>0 and y<10  # True
