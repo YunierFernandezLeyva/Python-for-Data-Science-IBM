5df256726ace97095483855c4f37307de60cbfd5
Yunier Fernández Leyva <yunierfdezl@gmail.com>
2026-01-14 20:33:31 +0100

    Create loops.py

---
+++ loops.py
@@ -0,0 +1,11 @@
+#Use loops to print out the elements in the list A:
+A=[3,4,5]
+for element in A:
+    print(element)
+
+#Find the value of  x  that will print out the sequence  1,2,..,10 :
+x=10
+y=1
+while(y != x):
+    print(f"El valor de y es: {y}" )
+    y=y+1
