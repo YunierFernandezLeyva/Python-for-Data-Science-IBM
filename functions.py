3b30201cde7f7cbce1236fa0000554bb3f1fa9be
Yunier Fernández Leyva <yunierfdezl@gmail.com>
2026-01-19 22:12:50 +0100

    Update

--- functions.py
+++ functions.py
@@ -1,5 +1,6 @@
 # First function example: Add 1 to a and store as b
 
+
 def add(a):
     """
     add 1 to a
@@ -59,3 +60,61 @@ x = 3
 # Makes function call and return function a y
 y = square(x)
 y
+
+# Directly enter a number as parameter
+
+square(2)
+
+# Function example
+
+def type_of_album(album, year_released):
+    
+    print(album, year_released)
+    if year_released > 1980:
+        return "Modern"
+    else:
+        return "Oldie"
+    
+x = type_of_album("The BodyGuard", 1980)
+print(x)
+
+
+
+#-------------------------------------Quiz on Functions---------------------------------
+
+#Come up with a function that divides the first input by the second input:
+def divide(num1, num2):
+    result = num1 / num2
+    return result
+
+#Use the function con for the following question.
+def con(a, b):
+    return (a + b)
+
+#Can the con function we defined before be used to add two integers or strings?
+#Yes, the con function can be used to add both integers and strings.
+print(con(2, 3))          # Adding two integers
+print(con("Hello, ", "World!"))  # Adding two strings   
+
+
+#Can the con function we defined before be used to concatenate lists or tuples?
+#Yes, the con function can be used to concatenate both lists and tuples.
+print(con([1, 2], [3, 4]))      # Concatenating two lists
+print(con((1, 2), (3, 4)))      # Concatenating two tuples
+
+
+#Write a function code to find total count of word little in the given string: 
+# "Mary had a little lamb Little lamb, little lamb Mary had a little lamb.
+# Its fleece was white as snow And everywhere that Mary went Mary went, Mary went Everywhere that Mary went The lamb was sure to go"**
+
+def count(text,passedkey):
+    words = []
+    words = text.split()
+    Dict = {}
+
+    for key in words:
+        if(key == passedkey):
+            Dict[key] = words.count(key)
+print("Total count",Dict)
+
+count("Mary had a little lamb Little lamb, little lamb Mary had a little lamb. Its fleece was white as snow And everywhere that Mary went Mary went, Mary went Everywhere that Mary went The lamb was sure to go", "little")
